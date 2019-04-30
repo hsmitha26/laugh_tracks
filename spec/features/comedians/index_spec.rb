0c7dd3df9f16de10364ec1e422feafea5be598b6
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-04-30 15:41:37 -0600

    adds before :each and test for specials

--- spec/features/comedians/index_spec.rb
+++ spec/features/comedians/index_spec.rb
@@ -2,21 +2,44 @@ require 'rails_helper'
 
 RSpec.describe "As a visitor, ", type: :feature do
   describe "I visit the comedians index page, " do
-    it "I see a list of comedians and their attributes" do
-      comedian_1 = Comedian.create!(name: "Comedian 1", age: 30, city: "Denver")
-      comedian_2 = Comedian.create!(name: "Comedian 2", age: 31, city: "Boulder")
+    before :each do
+      @comedian_1 = Comedian.create!(name: "Comedian 1", age: 30, city: "Denver")
+      @comedian_2 = Comedian.create!(name: "Comedian 2", age: 31, city: "Boulder")
+
+      @special_1 = @comedian_1.specials.create!(name: "Special 1", run_time: 55)
+      @special_2 = @comedian_1.specials.create!(name: "Special 2", run_time: 60)
+      @special_3 = @comedian_2.specials.create!(name: "Special 3", run_time: 65)
+    end
+    it "I see a list of comedians - their name, age and city" do
       visit '/comedians'
 
-      within "#comedian-index-#{comedian_1.id}" do
-        expect(page).to have_content(comedian_1.name)
-        expect(page).to have_content(comedian_1.age)
-        expect(page).to have_content(comedian_1.city)
+      within "#comedian-index-#{@comedian_1.id}" do
+        expect(page).to have_content(@comedian_1.name)
+        expect(page).to have_content(@comedian_1.age)
+        expect(page).to have_content(@comedian_1.city)
       end
 
-      within "#comedian-index-#{comedian_2.id}" do
-        expect(page).to have_content(comedian_2.name)
-        expect(page).to have_content(comedian_2.age)
-        expect(page).to have_content(comedian_2.city)
+      within "#comedian-index-#{@comedian_2.id}" do
+        expect(page).to have_content(@comedian_2.name)
+        expect(page).to have_content(@comedian_2.age)
+        expect(page).to have_content(@comedian_2.city)
+      end
+    end
+
+    it "I also see a list of each comedian's TV specials, including name of special and run time" do
+      visit '/comedians'
+
+      within "#comedian-specials-#{@special_1.id}" do
+        expect(page).to have_content(@special_1.name)
+        expect(page).to have_content(@special_1.run_time)
+      end
+      within "#comedian-specials-#{@special_2.id}" do
+        expect(page).to have_content(@special_2.name)
+        expect(page).to have_content(@special_2.run_time)
+      end
+      within "#comedian-specials-#{@special_3.id}" do
+        expect(page).to have_content(@special_3.name)
+        expect(page).to have_content(@special_3.run_time)
       end
     end
   end
