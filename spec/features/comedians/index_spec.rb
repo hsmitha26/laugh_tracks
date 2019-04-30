5b12d4e170c8772d403284ce44c866b72817a885
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-04-29 19:56:01 -0600

    test passing for comedians index page

--- spec/features/comedians/index_spec.rb
+++ spec/features/comedians/index_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 RSpec.describe "As a visitor, ", type: :feature do
   describe "I visit the comedians index page, " do
     it "I see a list of comedians and their attributes" do
-      @comedian_1 = Comedian.create!(name: "Comedian 1", age: 30, city: "Denver")
-      @comedian_2 = Comedian.create!(name: "Comedian 2", age: 31, city: "Boulder")
+      comedian_1 = Comedian.create!(name: "Comedian 1", age: 30, city: "Denver")
+      comedian_2 = Comedian.create!(name: "Comedian 2", age: 31, city: "Boulder")
       visit '/comedians'
 
-      within "#comedian-index-#{@comedian_1.id}" do
-        expect(page).to have_content(@comedian_1.name)
-        expect(page).to have_content(@comedian_1.age)
-        expect(page).to have_content(@comedian_1.city)
+      within "#comedian-index-#{comedian_1.id}" do
+        expect(page).to have_content(comedian_1.name)
+        expect(page).to have_content(comedian_1.age)
+        expect(page).to have_content(comedian_1.city)
       end
 
-      within "#comedian-index-#{@comedian_2.id}" do
-        expect(page).to have_content(@comedian_2.name)
-        expect(page).to have_content(@comedian_2.age)
-        expect(page).to have_content(@comedian_2.city)
+      within "#comedian-index-#{comedian_2.id}" do
+        expect(page).to have_content(comedian_2.name)
+        expect(page).to have_content(comedian_2.age)
+        expect(page).to have_content(comedian_2.city)
       end
     end
   end
