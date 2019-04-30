448a39f8880e6472591d208d327dbde5f86013ad
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-04-30 15:48:33 -0600

    passing relationship test

--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Special do
   describe "Validations" do
-    it {should have_presence_of :name}
-    it {should have_presence_of :run_time}
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :run_time}
+  end
+
+  describe "Relationship" do
+    it {should belong_to :comedian}
   end
 end
